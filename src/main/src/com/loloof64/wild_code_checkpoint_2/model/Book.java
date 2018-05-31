19cf2b6a9f8e0992fa04b66129782843000be101
laurent bernabé <Laurent_Bernabe_student2018@wilder.school>
2018-05-31 14:58:05 +0200

    Added book entity.

---
+++ src/main/src/com/loloof64/wild_code_checkpoint_2/model/Book.java
@@ -0,0 +1,155 @@
+package com.loloof64.wild_code_checkpoint_2.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    private Category category;
+
+    @Basic
+    private String title;
+
+    @Basic
+    private String author;
+
+    @Basic
+    private String publisher;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "publication_date")
+    private Date publicationDate;
+
+    @Basic
+    private String language;
+
+    @Basic
+    private Double price;
+
+    @Basic
+    private Integer quantity;
+
+    @Basic
+    private String isbn;
+
+    @Basic
+    @Column(name = "pages_count")
+    private Integer pagesCount;
+
+    @OneToOne
+    private Dimension dimension;
+
+    @OneToOne
+    private Weight weight;
+
+    public Long getId() {   
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getPagesCount() {
+        return pagesCount;
+    }
+
+    public void setPagesCount(Integer pagesCount) {
+        this.pagesCount = pagesCount;
+    }
+
+    public Dimension getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
+    }
+
+    public Weight getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Weight weight) {
+        this.weight = weight;
+    }
+}
